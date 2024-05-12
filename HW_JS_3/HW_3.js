3fd20b76ee9f12ae176b889e0af1a690f95537ad
apodk <an.podkolzina@gmasil.com>
2024-05-12 17:13:42 +0300

    HW_JS_3 final added

--- HW_JS_3/HW_3.js
+++ HW_JS_3/HW_3.js
@@ -121,9 +121,6 @@
 //Використовуючи данні з масиву, за допомоги document.write та циклу
 //побудувати структуру по шаблону
 //Масив:
-
-// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-
 //ШАБЛОН:
 //   <ul>
 //       <li>ITEM OF ARRAY</li>
@@ -136,6 +133,21 @@
 //   </ul>
 
 //замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write(`<ul>`);
+for (const item of listOfItems){
+    document.write(`
+    <li>
+        ${item}
+        </li> `);
+
+}
+document.write(`</ul>`);
+
+
+
 
 //-----------------------------------------------
 
